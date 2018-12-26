3098faada55116f0ab6a75caf63426e471d60b05
Yuto Nakano <yuto.44.bb@gmail.com>
2018-12-26 21:36:59 +0900

    Initial Commit

---
+++ Swift__paractice_manga/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift__paractice_manga
+//
+//  Created by 中野湧仁 on 2018/12/26.
+//  Copyright © 2018年 中野湧仁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
